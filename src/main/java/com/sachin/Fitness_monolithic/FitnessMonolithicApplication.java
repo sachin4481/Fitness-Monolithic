637f75b261d7a5f22831c04f9db31cda2d2bcfed
sachin4481 <sachin889023@gmail.com>
2025-12-27 00:28:24 +0530

    first commit

---
+++ src/main/java/com/sachin/Fitness_monolithic/FitnessMonolithicApplication.java
@@ -0,0 +1,13 @@
+package com.sachin.Fitness_monolithic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessMonolithicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessMonolithicApplication.class, args);
+	}
+
+}
